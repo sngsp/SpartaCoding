c85126e1327371a463b4ce61c391e8a6a4855ecd
sngsp <ssleerok@gmail.com>
2024-03-19 17:06:20 +0900

    lv.3 completed

--- Todo1/Todo1/TodoCell.swift
+++ Todo1/Todo1/TodoCell.swift
@@ -5,6 +5,10 @@
 //  Created by Seungseop Lee on 3/18/24.
 //
 
+
+// 그만 바꾸기
+
+
 import UIKit
 
 class TodoCell: UITableViewCell {
@@ -14,11 +18,65 @@ class TodoCell: UITableViewCell {
     @IBOutlet weak var todoName: UILabel!
     
     var switchChanged:((Bool)->Void)?
-    
+    // 추가된 속성: 가운데 선을 그을지 여부를 나타내는 속성
+        var centerLine: Bool = false {
+            didSet {
+                updateCenterLine()
+            }
+        }
+        
+        // 셀이 로드될 때 호출되는 메서드
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            updateCenterLine()
+        }
     
     @IBAction func switchButtonTapped(_ sender: UISwitch) {
+        
         switchChanged?(sender.isOn)
+        centerLine = sender.isOn
+        
+//        if centerLine == true {
+//            addCenterLine()
+//        } else {
+//            removeCenterLine()
+//        }
     }
-            
+    
+    // 가운데 선 업데이트하는 메서드
+        private func updateCenterLine() {
+            if centerLine {
+                let attributedString = NSMutableAttributedString(string: todoName.text ?? "")
+                attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSRange(location: 0, length: attributedString.length))
+                todoName.attributedText = attributedString
+            } else {
+                let attributedString = NSAttributedString(string: todoName.text ?? "")
+                todoName.attributedText = attributedString
+            }
+        }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        centerLine = false
+    }
+    
+//    // 줄 생성
+//    private func addCenterLine() {
+//        guard let text = todoName.text else { return }
+//        let attributedString = NSMutableAttributedString(string: text)
+//        attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSRange(location: 0, length: attributedString.length))
+//        todoName.attributedText = attributedString
+//        centerLine = true // 줄이 그려졌음을 표시
+//    }
+//    
+//    // 줄 삭제
+//    private func removeCenterLine() {
+//        guard let text = todoName.text else { return }
+//        let attributedString = NSAttributedString(string: text)
+//        todoName.attributedText = attributedString
+//        centerLine = false // 줄을 제거함을 표시
+//    }
+//    
+    
 }
 
--- Todo1/Todo1/ViewController.swift
+++ Todo1/Todo1/ViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-
+    
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
-
     var todoArray: [TodoInfo] = []
     
+    // 초기 설정
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
@@ -22,34 +22,35 @@ class ViewController: UIViewController, UITableViewDataSource {
         addButton.setTitle("추가하기", for: .normal)
     }
     
+    // '추가하기' 버튼 클릭 시 동작
     @IBAction func addButtonTapped(_ sender: UIButton) {
         let alertController = UIAlertController(title: "Todo 추가", message: "할 일을 입력하세요", preferredStyle: .alert)
-                
-                alertController.addTextField { textField in
-                    textField.placeholder = "할 일"
-                }
-                
-                let addAction = UIAlertAction(title: "추가", style: .default) { [weak self] _ in
-                    guard let textField = alertController.textFields?.first, let todoName = textField.text else {
-                        return
-                    }
-                    
-                    let newTodo = TodoInfo(todoId: self?.todoArray.count ?? 0 + 1, todoName: todoName, isCompleted: false)
-                    
-                    self?.todoArray.append(newTodo)
-                    self?.tableView.reloadData()
-                }
-                
-                let cancelAction = UIAlertAction(title: "취소", style: .cancel)
-                
-                alertController.addAction(addAction)
-                alertController.addAction(cancelAction)
-                
-                present(alertController, animated: true)
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "할 일"
+        }
+        
+        let addAction = UIAlertAction(title: "추가", style: .default) { [weak self] _ in
+            guard let textField = alertController.textFields?.first, let todoName = textField.text else {
+                return
+            }
+            
+            let newTodo = TodoInfo(todoId: self?.todoArray.count ?? 0 + 1, todoName: todoName, isCompleted: false)
+            
+            self?.todoArray.append(newTodo)
+            self?.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
         
     }
     
-    
+    // UITableViewDataSource 확장으로 인한 필수구현 함수 2개
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoArray.count
     }
@@ -58,18 +59,30 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoCell
         
-        cell.todoName.text = todoArray[indexPath.row].todoName
-        cell.switchButton.isOn = todoArray[indexPath.row].isCompleted
+        let todo = todoArray[indexPath.row]
+        
+        cell.todoName.text = todo.todoName
+        cell.switchButton.isOn = todo.isCompleted
         cell.switchChanged = { [weak self] isOn in
             self?.todoArray[indexPath.row].isCompleted = isOn
         }
+        cell.centerLine = todoArray[indexPath.row].isCompleted
         
         return cell
     }
     
-
-
+    // Delete 기능을 활성화하는 함수
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            todoArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            
+        }
+    }
+    
 }
-
-
 
